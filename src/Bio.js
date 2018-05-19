c70e0ee853433fc1c5e2ac231879d09cfc47c387
JordanFalcon <j.falcon122@gmail.com>
2018-05-19 12:36:35 -0400

    Fix spelling arror on Bio, Add active state for nav

--- src/Bio.js
+++ src/Bio.js
@@ -15,22 +15,22 @@ class Bio extends Component {
                 <h3>Experience</h3>
                 <ul className="Bio-experience">
                     <li>
-                        <h4><a href="http://gestalt.io/" target="_blank">Gesatalt</a> - Web Develiper Intern</h4>
+                        <h4><a href="http://gestalt.io/" target="_blank" title="Gesatalt" alt="Gesatalt">Gesatalt</a> - Web Developer Intern</h4>
                         <h5></h5>
                     </li>
                     <li>
-                        <h4><a href="https://www.nytimes.com/" target="_blank">New York Times</a> - Web Develiper Intern</h4>
+                        <h4><a href="https://www.nytimes.com/" target="_blank" title="New York Times" alt="New York Times">New York Times</a> - Web Developer Intern</h4>
                         <h5></h5>
                     </li>
                 </ul>
                 <h3>Education</h3>
                 <ul className="Bio-education">
                     <li> 
-                        <h4><a href="http://lehman.edu/" target="_blank">Lehman College</a> - June 2016 - Present</h4>
+                        <h4><a href="http://lehman.edu/" target="_blank" alt="Lehamn College" title="Lehamn College">Lehman College</a> - June 2016 - Present</h4>
                         <h5>Computer Sceince - Bachelors Sceince - Senior</h5>
                     </li>  
                     <li> 
-                        <h4><a href="https://flatironschool.com/" target="_blank">The Flatiron School</a> January 2015 - June 2015</h4>
+                        <h4><a href="https://flatironschool.com/" target="_blank" alt="The Flatiron School" title="The Flatiron School">The Flatiron School</a> - January 2015 - June 2015</h4>
                         <h5>Web immersive program - Graduated</h5>
                     </li>  
                 </ul>
--- src/NavBar.js
+++ src/NavBar.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { BrowserRouter as Router, Link } from 'react-router-dom'
+import { BrowserRouter as Router, Link, NavLink } from 'react-router-dom'
 import logo from './jf-logo.svg';
 import './NavBar.css';
 import Bio from './Bio'
@@ -8,14 +8,14 @@ import Contact from './Contact'
 
 class NavBar extends Component {
   render() {
+
     return (
         <div className="NavBar"> 
           <div className="NavBar-container"> 
             <Link to={"/Bio"}><img src={logo} className="Nav-loading-logo" alt="logo" /></Link>
-
             <ul>
-                <li><Link to={"/Bio"} className="Nav-list-item">Bio</Link></li>
-                <li><Link to={"/Projects"} className="Nav-list-item">Projects</Link></li>
+                <li><NavLink to={"/Bio"} className="Nav-list-item Nav-list-item-active">Bio</NavLink></li>
+                <li><NavLink to={"/Projects"} className="Nav-list-item" >Projects</NavLink></li>
             </ul>
           </div>
         </div>
