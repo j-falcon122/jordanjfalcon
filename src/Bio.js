e51b27ef1fd68317c0dfc33ef98c466fea7d961b
JordanFalcon <j.falcon122@gmail.com>
2018-05-26 07:52:51 -0400

    Update bio content

--- src/Bio.js
+++ src/Bio.js
@@ -15,22 +15,26 @@ class Bio extends Component {
                 <h3>Experience</h3>
                 <ul className="Bio-experience">
                     <li>
-                        <h4><a href="http://gestalt.io/" target="_blank" title="Gesatalt" alt="Gestalt">Gestalt</a> - Web Developer Intern</h4>
-                        <h5></h5>
+                        <h4><a href="http://www.espn.com/" target="_blank" title="Gesatalt" alt="ESPN">ESPN</a> - Front End Software Engineer Intern (Current)</h4>
                     </li>
                     <li>
-                        <h4><a href="https://www.nytimes.com/" target="_blank" title="New York Times" alt="New York Times">New York Times</a> - Web Developer Intern</h4>
-                        <h5></h5>
+                        <h4><a href="http://gestalt.io/" target="_blank" title="Gesatalt" alt="Gestalt">Gestalt</a> - Web Developer (October-November 2017)</h4>
+                    </li>
+                    <li>
+                        <h4><a href="http://gestalt.io/" target="_blank" title="Gesatalt" alt="Gestalt">Gestalt</a> - Web Developer Intern (Summer 2017) </h4>
+                    </li>
+                    <li>
+                        <h4><a href="https://www.nytimes.com/" target="_blank" title="New York Times" alt="New York Times">New York Times</a> - Web Developer Intern (Summer 2015)</h4>
                     </li>
                 </ul>
                 <h3>Education</h3>
                 <ul className="Bio-education">
                     <li> 
-                        <h4><a href="http://lehman.edu/" target="_blank" alt="Lehamn College" title="Lehamn College">Lehman College</a> - June 2016 - Present</h4>
+                        <h4><a href="http://lehman.edu/" target="_blank" alt="Lehamn College" title="Lehamn College">Lehman College</a> (June 2016 - Present)</h4>
                         <h5>Computer Science - Bachelors Science - Senior</h5>
                     </li>  
                     <li> 
-                        <h4><a href="https://flatironschool.com/" target="_blank" alt="The Flatiron School" title="The Flatiron School">The Flatiron School</a> - January 2015 - June 2015</h4>
+                        <h4><a href="https://flatironschool.com/" target="_blank" alt="The Flatiron School" title="The Flatiron School">The Flatiron School</a>  (January 2015 - June 2015)</h4>
                         <h5>Web immersive program - Graduated</h5>
                     </li>  
                 </ul>
