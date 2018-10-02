bf9f81479219d95bd82f3908e735b8126380f7ea
JordanFalcon <j.falcon122@gmail.com>
2018-10-02 18:32:43 -0400

    Update Resume link

--- src/Footer.js
+++ src/Footer.js
@@ -9,7 +9,7 @@ class Footer extends Component {
         <ul className="Footer-assets">
           <li><a href="https://www.linkedin.com/in/jordanfalcon/" target="_blank" rel="noopener noreferrer"><i className="fab fa-linkedin" aria-hidden="true" id="nav-link"></i></a></li>
           <li><a href="https://github.com/j-falcon122" target="_blank" rel="noopener noreferrer"><i className="fab fa-github-alt" aria-hidden="true" id="nav-link"></i></a></li>
-          <li><a href="https://www.dropbox.com/s/5t80o6k57g46ty7/JF_Resume.pdf?dl=0" target="_blank"><i className="fa fa-file-pdf" aria-hidden="true" id="nav-link"></i></a></li>
+          <li><a href="https://www.dropbox.com/s/sh23475mg9ampkb/JF_Resume_10_2_2018.pdf?dl=0" target="_blank"><i className="fa fa-file-pdf" aria-hidden="true" id="nav-link"></i></a></li>
           <li><a href="mailto:j.falcon122@gmail.com"><i className="far fa-envelope" aria-hidden="true" id="nav-link"></i></a></li>
           </ul>
       </div>
